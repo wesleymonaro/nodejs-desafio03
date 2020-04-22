d511998ec47697a4e28ccd7eaf7ed1d446dc5588
Wesley Monaro <wesleymonaro@hotmail.com>
2020-04-22 17:18:07 -0300

    finish project

--- src/database/migrations/1587565120983-AddForeignKeyToCategories.ts
+++ src/database/migrations/1587565120983-AddForeignKeyToCategories.ts
@@ -1,22 +1,37 @@
-import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableForeignKey,
+  TableColumn,
+} from 'typeorm';
 
 export default class AddForeignKeyToCategories1587565120983
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'transactions',
+      new TableColumn({
+        name: 'category_id',
+        type: 'uuid',
+        isNullable: true,
+      }),
+    );
+
     await queryRunner.createForeignKey(
       'transactions',
       new TableForeignKey({
-        name: 'TransactionCategory',
         columnNames: ['category_id'],
         referencedColumnNames: ['id'],
         referencedTableName: 'categories',
-        onDelete: 'SET NULL',
+        name: 'TransactionCategory',
         onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
       }),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+    await queryRunner.dropColumn('transactions', 'category_id');
   }
 }
--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -13,18 +13,32 @@ class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
     const transactions = await this.find();
 
-    let income = 0;
-    let outcome = 0;
-    let total = 0;
-    transactions.forEach(t => {
-      if (t.type === 'income') {
-        income += t.value;
-        total += t.value;
-      } else if (t.type === 'outcome') {
-        outcome += t.value;
-        total -= t.value;
-      }
-    });
+    const { income, outcome } = transactions.reduce(
+      (accumulator, transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += Number(transaction.value);
+            break;
+
+          case 'outcome':
+            accumulator.outcome += Number(transaction.value);
+            break;
+
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
     return { income, outcome, total };
   }
 }
